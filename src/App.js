521d43f631fdef4c06e89695f15b9f72a8e06d98
tair12 <tair-00@list.ru>
2022-07-15 12:06:35 +0300

    Dz1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+function AboutUs ( props){
+   return(
+          <div>
+            <h1>
+               AboutUs
+            </h1>
+          </div>
+   )
+}
+
+
+
 
 function App(){
    return(
-     <div> HEllo world</div>
+     <div className = "container"> 
+     <AboutUs title=" Git"/>
+     </div>
    );
 }
 export default App;
